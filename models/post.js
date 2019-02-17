0429579bcaf4d74c3d5adf9999848c71fd711344
Jungdo Lee <lee.jungdo@gmail.com>
2019-02-17 18:19:13 +0900

    Change content's data type to text

--- models/post.js
+++ models/post.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false
       },
       content: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
       }
     },
