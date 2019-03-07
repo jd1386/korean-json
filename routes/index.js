018a146375bba3fa8821de57c9e4b9d44139b528
sunjae-kim <sunjae-kim@gmail.com>
2019-03-08 00:22:04 +0900

    Implement paragraph route
     - Create paragraphs data
     - Create `randomParagraph` method in util
     - Create `GET /paragraph` route

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ import usersRouter from './users';
 import postsRouter from './posts';
 import todosRouter from './todos';
 import commentsRouter from './comments';
+import paragraphRouter from './paragraph';
 
-export { usersRouter, postsRouter, todosRouter, commentsRouter };
+export { usersRouter, postsRouter, todosRouter, commentsRouter, paragraphRouter };
